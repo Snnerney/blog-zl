efed8a8a1dd099ed1547ca2b4bbf647f0ea2ed11
antiss <lwwrr2004@gmail.com>
2026-09-07 03:47:53 +0800

    fix: 添加新的更新日志条目，记录使用 blog.noospic.top 域名的变更

--- app/components/widget/BlogLog.vue
+++ app/components/widget/BlogLog.vue
@@ -3,6 +3,11 @@ const { timeEstablished } = useAppConfig()
 
 const blogLog = [
 	{
+		label: '2026-09-07',
+		value: '使用 blog.noospic.top 域名',
+		tip: `本来想将github.io重定向到域名，但是302代价又太大，后续如果再次替换域名，重定向的缓存时间未知`,
+	},
+	{
 		label: '2026-08-31',
 		value: 'feat: 聚合 Bangumi 番剧/影视/游戏收藏页，借鉴自@blog.shinya.click',
 		tip: `借鉴自 https://blog.shinya.click/media`,
